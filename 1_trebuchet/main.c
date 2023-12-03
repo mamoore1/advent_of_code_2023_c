d1962ce7ab16b5fc3452e99af38c14e9d9f23dd3
mamoore1 <mick.moore@hotmail.com>
2023-12-03 18:20:39 +0000

    puzzle 1 part 2

--- 1_trebuchet/main.c
+++ 1_trebuchet/main.c
@@ -6,10 +6,14 @@
 // NB. if there is only one digit in the line, that counts as both the 
 // first and last value
 
+// Part two: some of the calibration values have been written in text form,
+// e.g., 8 as eight. Parse these values as well.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <string.h>
 
 #define LINE_LENGTH (256)
 
@@ -26,9 +30,29 @@ int main(void) {
         return 1;
     }
 
+    // This is pretty hacky, but we can just iterate through all of the digits
+    // and try and to find and replace them
+    // (It would be a lot more efficient to do this for the whole file,
+    // rather than line by line.)
+    
+    const char *digit_strings[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
     while (fgets(line, LINE_LENGTH, file)) {
         int first_digit = -1, last_digit;
         char *p = line;
+
+        for (int i = 0; i < 9; i++) {        
+            char *index;
+            
+            // As a hack, we can just replace the second
+            // character of the substring with the relevant integer
+            // (Can't overwrite first character as this will mean numbers can overwrite 
+            // numbers that appear before them in the line, making them 
+            // undetectable)
+            while (index = strstr(line, digit_strings[i])) {
+                    *(index + 1) = '0' + (i + 1);
+            }
+        }
 
         while (*p) {
             if (isdigit(*p)) {
